cab733c02586c4d82d5dba577034fcad5ce02657
Olli P <ollip@outlook.com>
2022-12-15 14:44:59 +0200

    fix unwrapped test

--- __tests__/get-test.js
+++ __tests__/get-test.js
@@ -2,20 +2,21 @@ import get from "./../src/get.js";
 
 describe("function get", () => {
     // test if you get the value ['a,0,b,c'] or ['a', '0', 'b', 'c'] from object
-    it("should ..", () => {
+    it("should return correct object", () => {
         const object = { 'a': [{ 'b': { 'c': 3 } }] };
         expect(get(object, 'a[0].b.c')).toEqual(3);
     });
 
     // test if you get correct value when default is present.
-    it("should ..", () => {
+    it("should not return default value, if it object is returnable", () => {
         const object2 = { 'a': [{ 'b': { 'c': 0 } }] };
         const undefinedVal = undefined;
         expect(get(object2, ['a','0','b','c'], undefinedVal)).not.toEqual(undefined);
     });
-
-    // test if you get default value from object, with undefined resolved values
-    const object2 = { 'a': [{ 'b': { 'c': 0 } }] };
-    const undefinedVal = undefined;
-    expect(get(object2, ['a,b,c'], undefinedVal)).toEqual(undefined);
+    it("should return return default, if value is not found", () => {
+        // test if you get default value from object, with undefined resolved values
+        const object2 = { 'a': [{ 'b': { 'c': 0 } }] };
+        const undefinedVal = undefined;
+        expect(get(object2, ['a,b,c'], undefinedVal)).toEqual(undefined);
+    });
 });
