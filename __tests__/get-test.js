82a0afdd9b5931b33baab61da80cac388dafb46d
Olli P <ollip@outlook.com>
2022-12-15 14:40:35 +0200

    tests for helper get

--- __tests__/get-test.js
+++ __tests__/get-test.js
@@ -1,11 +1,21 @@
 import get from "./../src/get.js";
 
 describe("function get", () => {
-    // test if you get the value ['a', '0', 'b', 'c'] from object
+    // test if you get the value ['a,0,b,c'] or ['a', '0', 'b', 'c'] from object
     it("should ..", () => {
+        const object = { 'a': [{ 'b': { 'c': 3 } }] };
+        expect(get(object, 'a[0].b.c')).toEqual(3);
+    });
 
+    // test if you get correct value when default is present.
+    it("should ..", () => {
+        const object2 = { 'a': [{ 'b': { 'c': 0 } }] };
+        const undefinedVal = undefined;
+        expect(get(object2, ['a','0','b','c'], undefinedVal)).not.toEqual(undefined);
     });
 
     // test if you get default value from object, with undefined resolved values
-    
+    const object2 = { 'a': [{ 'b': { 'c': 0 } }] };
+    const undefinedVal = undefined;
+    expect(get(object2, ['a,b,c'], undefinedVal)).toEqual(undefined);
 });
