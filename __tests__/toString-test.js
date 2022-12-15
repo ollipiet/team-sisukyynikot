1fb96920dfc999219113331edfe02f3462ab237b
Olli P <ollip@outlook.com>
2022-12-15 15:28:44 +0200

    some tests

--- __tests__/toString-test.js
+++ __tests__/toString-test.js
@@ -1,7 +1,17 @@
 import toString from "./../src/toString.js";
 
 describe("function toString", () => {
-    it("should ..", () => {
 
+    // Test if toString converts null to nothing
+    it("should ..", () => {
+        expect(toString(null)).toEqual('');
     });
+
+    it("converts values like -1 and -0 to correct value", () => {
+        expect(toString(-0)).toEqual('-0');
+    });
+
+    it("converts values in a list to correct string", () => {
+        expect(toString([1,2,3])).toEqual('1,2,3');
+    })
 });
