bc58547e0c74d0181dd7724ad8ea03d9b21d63e7
Eero Häihälä <eero.haihala@gmail.com>
2022-12-16 12:02:19 +0200

    Refactor existing tests based on docstrings

--- __tests__/toString-test.js
+++ __tests__/toString-test.js
@@ -1,17 +1,14 @@
 import toString from "./../src/toString.js";
 
-describe("function toString", () => {
-
-    // Test if toString converts null to nothing
-    it("should ..", () => {
-        expect(toString(null)).toEqual('');
+describe("toString", () => {
+  const values = [
+    { input: null, output: "" },
+    { input: -0, output: "-0" },
+    { input: [1, 2, 3], output: "1,2,3" },
+  ];
+  for (const { input, output } of values) {
+    it(`toString(${input}) should be "${output}" like in docstring`, () => {
+      expect(toString(input)).toEqual(output);
     });
-
-    it("converts values like -1 and -0 to correct value", () => {
-        expect(toString(-0)).toEqual('-0');
-    });
-
-    it("converts values in a list to correct string", () => {
-        expect(toString([1,2,3])).toEqual('1,2,3');
-    })
+  }
 });
